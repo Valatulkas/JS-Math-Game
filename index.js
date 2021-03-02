4808ef4578f471e2f341bd2a8cabaa398f7fbc3d
J <>
2021-03-02 17:49:33 -0500

    fixed time remaining display, need to add styling

--- index.js
+++ index.js
@@ -1,70 +1,68 @@
-
-$(document).ready(function () {
+$(document).ready(function(){
   var currentQuestion;
+  var interval;
+  var timeLeft = 10;
   var score = 0;
-  var timeRemaining = 10;
-  var countDown;
+
+  var updateTimeLeft = function (amount) {
+    timeLeft += amount;
+    $('#time-left').text(timeLeft);
+  };
 
   var updateScore = function (amount) {
     score += amount;
     $('#score').text(score);
   };
 
-  var updateTimeRemaining = function (amount) {
-    timeRemaining += amount;
-    $('#time-left').text(timeRemaining);
-  }
-
   var startGame = function () {
-    if (!countDown) {
+    if (!interval) {
       if (timeLeft === 0) {
         updateTimeLeft(10);
         updateScore(-score);
       }
-      countDown = setInterval(function () {
-        updateTimeRemaining(-1);
-        if (timeRemaining === 0) {
-          clearInterval(countDown);
-          countDown = undefined;
+      interval = setInterval(function () {
+        updateTimeLeft(-1);
+        if (timeLeft === 0) {
+          clearInterval(interval);
+          interval = undefined;
         }
       }, 1000);
     }
-  }
+  };
 
-  var randomNum = function (size) {
+  var randomNumberGenerator = function (size) {
     return Math.ceil(Math.random() * size);
   };
 
   var questionGenerator = function () {
     var question = {};
-    var numOne = randomNum(10);
-    var numTwo = randomNum(10);
-
-    question.answer = numOne + numTwo;
-    question.equation = String(numOne) + '+' + String(numTwo);
+    var num1 = randomNumberGenerator(10);
+    var num2 = randomNumberGenerator(10);
+    
+    question.answer = num1 + num2;
+    question.equation = String(num1) + " + " + String(num2);
 
     return question;
-  }
+  };
 
-  var createNewQuestion = function () {
+  var renderNewQuestion = function () {
     currentQuestion = questionGenerator();
-    $("#equation").text(currentQuestion.equation);
-  }
+    $('#equation').text(currentQuestion.equation);
+  };
 
   var checkAnswer = function (userInput, answer) {
     if (userInput === answer) {
-      createNewQuestion();
+      renderNewQuestion();
       $('#user-input').val('');
-      updateTimeRemaining(+1);
+      updateTimeLeft(+1);
       updateScore(+1);
     }
-  }
+  };
 
-  $('#userInput').on('keyup', function () {
+  $('#user-input').on('keyup', function () {
     startGame();
     checkAnswer(Number($(this).val()), currentQuestion.answer);
-  })
+  });
 
-  createNewQuestion();  
-
+  renderNewQuestion();
 });
